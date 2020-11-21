0d176d3c16613cdf0cd3321103f968b1f0d88626
Almira Fajardo <almirabiancafajardo@gmail.com>
2020-11-21 16:46:13 +0800

    Adding the project

---
+++ src/cal_demo/CalDemo.java
@@ -0,0 +1,23 @@
+package cal_demo;
+
+import java.util.Scanner;
+
+public class CalDemo {
+
+	public static void main(String[] args) {
+		int num1,num2;
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		num1 = in.nextInt();
+		
+		System.out.println("Enter 2nd number: ");
+		num2 = in.nextInt();
+		
+		System.out.println("Sum = "+(num1+num2));
+		System.out.println("Subtraction = "+(num1-num2));
+		System.out.println("Multiplication = "+(num1*num2));
+		System.out.println("Division = "+(num1/num2));
+	}
+
+}
